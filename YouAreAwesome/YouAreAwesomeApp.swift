da9e58362c4213a699dd086579a59f3d2c3e65fc
Hyunjin Kim <hyun.jin.kim.1112@gmail.com>
2026-01-11 21:13:20 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Hyunjin Kim on 11/1/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
